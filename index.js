9f1df68f06d2a1d74e1860bf52952ea440aa6f1d
Anakinyt <snowddon@gmail.com>
2025-07-14 10:45:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -113,6 +113,9 @@ app.get("/gamepasses", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Servidor online na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor online na porta ${PORT}`);
 });
+
